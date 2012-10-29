3f1741bb897eed76b4a71800ed5aa88817b26732
Peter Uittenbroek <uittenbroek@goldmund-wyldebeast-wunderliebe.com>
2012-10-29 16:31:12 +0100

    Add ability to grant local roles

--- src/wsapi4plone/user/browser/app.py
+++ src/wsapi4plone/user/browser/app.py
@@ -16,8 +16,64 @@ class ApplicationAPI(WSAPI):
 
     def get_user(self, username):
         user = api.user.get(username=username)
-        return user
+        roles = api.user.get_roles(username=username)
+        results = {}
+
+
+        results['username'] = user.id
+        results['fullname'] = user.getProperty('fullname')
+        results['email'] = user.getProperty('email')
+        results['roles'] = roles
+        return results
 
 
     def post_user(self, params):
-        pass
+
+
+        assert type(params) == dict, "The first agument must be a dictionary."
+
+
+        properties = {}
+        roles=('Member', )
+        if params.has_key('fullname'):
+            properties['fullname'] = params.get('fullname')
+        if params.has_key('location'):
+            properties['location'] = params.get('location')
+        if params.has_key('roles'):
+            roles = params.get('roles')
+
+        assert params.has_key('username'), "Missing username"
+        assert params.has_key('email'), "Missing e-mail"
+
+        membercreation = {
+                'username':params.get('username'),
+                'email':params.get('email'),
+                'roles': roles,
+                'properties':properties
+                }
+        try:
+            api.user.create(**membercreation)
+            self.logger.info("- post_user- Creating user %s " % (params))
+
+            return self.get_user(params.get('username'))
+        except Exception, e:
+            return e
+
+    def grant_user_roles(self, params):
+        assert type(params) == dict, "The first agument must be a dictionary."
+        assert params.has_key('roles'), "No roles given to grant"
+        assert params.has_key('username'), "No users given to grant to"
+
+        username = params.get('username')
+        roles = params.get('roles')
+
+        api.user.grant_roles(
+                username=username,
+                roles=roles,
+                obj=self.context
+                )
+
+        return api.user.get_permissions(username=username,obj=self.context)
+
+
+
